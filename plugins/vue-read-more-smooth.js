83369c2003b46278bbedb4ef738f55c0eff9adb9
m-n-taylor <michael.n.taylor.biz@gmail.com>
2020-05-02 12:30:00 -0400

    Add comments to vue-read-more-smooth.js

--- plugins/vue-read-more-smooth.js
+++ plugins/vue-read-more-smooth.js
@@ -3,3 +3,5 @@ import VueReadMoreSmooth from "vue-read-more-smooth"
 Vue.component('VueReadMoreSmooth', VueReadMoreSmooth)
 
 // update: 2025-07-31T19:35:11.145232
+
+// update: 2025-07-31T19:40:22.746041
